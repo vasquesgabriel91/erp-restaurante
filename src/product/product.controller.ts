ad2890931de25caa38591121ebbc08aed28ce485
vasquesgabriel91 <vasquesgabriel91@gmail.com>
2026-05-01 14:57:12 -0300

    feate(Delete): add delete for crud product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ProductUseCase } from './useCases/ProductUseCase';
 import { CreateProductDto } from './dto/CreateProductDto';
+import { Product } from '@prisma/client';
 
 @Controller('product')
 export class ProductController {
@@ -19,5 +20,16 @@ export class ProductController {
   @Get()
   async getAll() {
     return this.ProductUseCase.getAll();
+  }
+
+  // @Put(':id'){
+  //   async update(@Param('id') id: string, @Body() body: CreateProductDto): Promise<Product> {
+  //     return this.ProductUseCase.update(id, body);
+  //   }
+  // }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Product> {
+    return this.ProductUseCase.delete(id);
   }
 }
--- src/product/useCases/ProductUseCase.ts
+++ src/product/useCases/ProductUseCase.ts
@@ -32,7 +32,7 @@ export class ProductUseCase {
     });
   }
 
-  async getById(id: string) {
+  async getById(id: string): Promise<Product> {
     const product = await this.productRepository.findById(id);
 
     if (!product) {
@@ -42,7 +42,33 @@ export class ProductUseCase {
     return product;
   }
 
-  async getAll() {
+  async getAll(): Promise<Product[]> {
     return this.productRepository.findAll();
+  }
+
+  async update(id: string, data: CreateProductDto): Promise<Product> {
+    const product = await this.productRepository.findById(id);
+    if (!product) throw new NotFoundException('Produto não encontrado');
+
+    const name = data.name.trim().toLowerCase();
+
+    const validUnits = ['kg', 'g', 'un', 'l', 'ml'];
+    if (!validUnits.includes(data.unit_measurement))
+      throw new BadRequestException('Unidade de medida inválida');
+    if (data.current_quantity < data.minimum_quantity)
+      throw new BadRequestException(
+        'Quantidade atual não pode ser menor que a mínima',
+      );
+
+    return this.productRepository.createProduct({
+      ...data,
+      name,
+    });
+  }
+
+  async delete(id: string): Promise<Product> {
+    const product = await this.productRepository.findById(id);
+    if (!product) throw new NotFoundException('Produto não encontrado');
+    return this.productRepository.deleteProduct(id);
   }
 }
